1eaca531d01befb1c2a015641ee230bc973579ab
Satvik Racharla <sracharla014@gmail.com>
2026-04-12 16:57:25 -0600

    moved all info up one directory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <section id='center'>
-        <div className='hero bg-red-800'>
+        <div className='hero bg-red-100'>
           <img src={heroImg} className='base' width='170' height='179' alt='' />
           <img src={reactLogo} className='framework' alt='React logo' />
           <img src={viteLogo} className='vite' alt='Vite logo' />
